30e65bd87ffe0d2588806073960f12c36d40576d
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-28 00:47:34 +0200

    crack without salt

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,10 @@
 import hashlib
 
+res = open("top-10000-passwords.txt").read().split("\n")
+
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    for i in range(10000):
+        hash_c = hashlib.sha1(res[i].encode('latin-1')).hexdigest()
+        if hash == hash_c:
+            return res[i]
+    return 'PASSWORD NOT IN DATABASE'
