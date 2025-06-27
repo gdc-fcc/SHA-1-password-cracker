7c201c34f1f6b42d3d806d548e81288355a135f7
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-28 01:06:56 +0200

    finalize exercise

--- password_cracker.py
+++ password_cracker.py
@@ -1,10 +1,20 @@
 import hashlib
 
-res = open("top-10000-passwords.txt").read().split("\n")
+passwords = open("top-10000-passwords.txt").read().split("\n")
+salts = open("known-salts.txt").read().split("\n")
+
+def hash1(password):
+    return hashlib.sha1(password.encode()).hexdigest()
 
 def crack_sha1_hash(hash, use_salts = False):
-    for i in range(10000):
-        hash_c = hashlib.sha1(res[i].encode('latin-1')).hexdigest()
-        if hash == hash_c:
-            return res[i]
+    for password in passwords:
+        if hash == hash1(password):
+            return password
+    if use_salts:
+        for password in passwords:
+            for salt in salts:
+                if hash == hash1(salt + password):
+                    return password
+                if hash == hash1(password+salt):
+                    return password
     return 'PASSWORD NOT IN DATABASE'
